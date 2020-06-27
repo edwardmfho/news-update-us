a399f26de9e0d871fefcfb227ee6e475267df69e
Edward Ho <mfho92@gmail.com>
2020-06-27 16:16:49 +1000

    modified the code to log error, and change running function

--- news_bot.py
+++ news_bot.py
@@ -1,6 +1,10 @@
+import os
+import sys
+import logging
+
 from telegram.ext import Updater, CommandHandler, CallbackContext
 from telegram import Update, ParseMode
-import os
+
 
 import requests
 import re
@@ -8,23 +12,34 @@ import re
 from get_news import headline
 from translate import translate_text
 
+logging.basicConfig(level=logging.INFO,format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+logger = logging.getLogger()
+
 MODE = os.getenv("MODE")
 TOKEN = os.getenv("TOKEN")
 
-def run(updater):
-        PORT = int(os.environ.get("PORT", "8443"))
-        HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME")
-        # Code from https://github.com/python-telegram-bot/python-telegram-bot/wiki/Webhooks#heroku
-        updater.start_webhook(listen="0.0.0.0",
-                              port=PORT,
-                              url_path=TOKEN)
-        updater.bot.set_webhook("https://{}.herokuapp.com/{}".format(HEROKU_APP_NAME, TOKEN))
 
 
-def start_handler(bot, update):
+if mode == "dev":
+	def run(updater):
+		updater.start_polling()
+elif mode=="prod":
+	def run(updater):
+	        PORT = int(os.environ.get("PORT", "8443"))
+	        HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME")
+	        # Code from https://github.com/python-telegram-bot/python-telegram-bot/wiki/Webhooks#heroku
+	        updater.start_webhook(listen="0.0.0.0",
+	                              port=PORT,
+	                              url_path=TOKEN)
+	        updater.bot.set_webhook("https://{}.herokuapp.com/{}".format(HEROKU_APP_NAME, TOKEN))
+else:
+	logger.error("No MODE sepcified!")
+	sys.exit(1)
+
+def start_handler(update: Update, context: CallbackContext):
     # Creating a handler-function for /start command 
     logger.info("User {} started bot".format(update.effective_user["id"]))
-    update.message.reply_text("Hello from Python!\nPress /random to get random number")
+    context.bot.update.message.reply_text("Hello from Python!\nPress /random to get random number")
 
 
 def news(update: Update, context: CallbackContext):
@@ -48,17 +63,17 @@ def news(update: Update, context: CallbackContext):
                  parse_mode=ParseMode.HTML)
 
 
-def main():
-	
-	updater = Updater(TOKEN, use_context=True)
-	dp = updater.dispatcher
 
-	dp.add_handler(CommandHandler('news', news))
-	updater.start_polling()
-
-	updater.idle()
 
 if __name__ == '__main__':
-	main()	
+	logger.info("Starting Bot")
+	updater = Updater(TOKEN, use_context=True)
+	
+	dp = updater.dispatcher
+	dp.add_handler(CommandHandler('start', start_handler))
+	dp.add_handler(CommandHandler('news', news))
+
+	run(updater)
+
 
 
